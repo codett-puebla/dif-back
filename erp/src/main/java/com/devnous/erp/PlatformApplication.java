d7d5a6f5ab87a2a44b849f1b2883a624d27c923b
AlexMatix <alexrodriguezserrano1595@gmail.com>
2019-11-15 18:13:59 -0600

    change string data base connection

--- erp/src/main/java/com/devnous/erp/PlatformApplication.java
+++ erp/src/main/java/com/devnous/erp/PlatformApplication.java
@@ -2,13 +2,14 @@ package com.devnous.erp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 import javax.annotation.PostConstruct;
 import java.util.Date;
 import java.util.TimeZone;
 
 @SpringBootApplication
-public class PlatformApplication {
+public class PlatformApplication extends SpringBootServletInitializer {
 
     @PostConstruct
     public void init() {
